7d5776156d7ddd9286c65ddc8fd9f68348bdc3f2
shruthss <146167826+shruthss@users.noreply.github.com>
2025-04-25 15:03:30 +0530

    Delete dao directory

--- dao/CourierAdminServiceCollectionImpl.py
+++
@@ -1,9 +0,0 @@
-from dao.CourierUserServiceCollectionImpl import CourierUserServiceCollectionImpl
-from dao.ICourierAdminService import ICourierAdminService
-
-class CourierAdminServiceCollectionImpl(CourierUserServiceCollectionImpl, ICourierAdminService):
-    def viewAllCouriers(self):
-        return self.companyObj.getAllCouriers()
-
-    def viewAllEmployees(self):
-        return self.companyObj.getAllEmployees()
--- dao/CourierAdminServiceImpl.py
+++
@@ -1,9 +0,0 @@
-from dao.CourierUserServiceImpl import CourierUserServiceImpl
-from dao.ICourierAdminService import ICourierAdminService
-
-class CourierAdminServiceImpl(CourierUserServiceImpl, ICourierAdminService):
-    def viewAllCouriers(self):
-        return self.companyObj.getAllCouriers()
-
-    def viewAllEmployees(self):
-        return self.companyObj.getAllEmployees()
