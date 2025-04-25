ee91e9d49a3205679b153a728d4355cf1d7f4ef9
shruthss <146167826+shruthss@users.noreply.github.com>
2025-04-25 15:04:02 +0530

    Delete exception directory

--- exception/InvalidEmployeeIdException.py
+++
@@ -1,5 +0,0 @@
-# exception/InvalidEmployeeIdException.py
-
-class InvalidEmployeeIdException(Exception):
-    def __init__(self, message="Invalid Employee ID. Employee does not exist."):
-        super().__init__(message)
--- exception/TrackingNumberNotFoundException.py
+++
@@ -1,5 +0,0 @@
-# exception/TrackingNumberNotFoundException.py
-
-class TrackingNumberNotFoundException(Exception):
-    def __init__(self, message="Tracking number not found."):
-        super().__init__(message)
